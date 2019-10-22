7343615c163b28fecd0f556e4834b3baa63d6aa6
Abdulkader1992 <ebdo93@gmail.com>
2019-10-22 13:31:57 +0200

    Verbindung zur Page 1 erstellt

--- app/src/main/java/com/example/vraj/MainActivity.java
+++ app/src/main/java/com/example/vraj/MainActivity.java
@@ -32,8 +32,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             case R.id.btnAnmelden:
                 // Anmelden
-                Intent intent = new Intent(MainActivity.this, Page1.class);
-                startActivity(intent);
+            //    Intent intent = new Intent(MainActivity.this, Page1.class);
+            //    MainActivity.startActivity(intent);
+                startActivity(new Intent(getApplicationContext(), Page1.class));
                 break;
             case R.id.btnSchlissen:
                 finish();
@@ -41,7 +42,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btnFusszeile:
                 //Konrakte
                 break;
-            default:
                 //Alternative
         }
     }
